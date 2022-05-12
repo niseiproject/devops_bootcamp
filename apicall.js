c4c2c6c4afc0337aca50aa3caee08f6631ba662b
Jesus Gomez <jesus.gomez@dell.com>
2022-05-12 11:33:15 +0200

    Adding API call to fetch NASA pics :).

---
+++ apicall.js
@@ -0,0 +1,14 @@
+<!-- JS to gather data from NASA -->
+<script>
+    fetch('https://api.nasa.gov/planetary/apod?api_key=30wKykhzibTOdpJo9xJ0waYUDXLdfBxfXBZE5SEr')
+    .then(response => response.json())
+    .then (function(data){
+        console.log(data);
+        document.getElementById("nasa_img").style = "background-image: url("+data.url+")";
+        document.getElementById("pic_text").innerHTML = data.explanation;
+    })
+    .catch(function(error) {
+        console.log("This is an ERROR");
+    });
+</script>
+<noscript>Sorry, your browser does not support JavaScript!!</noscript>
